8e8254766be97982796e3e8667ef341f65f3ec4c
wnstjd9701 <wnstjd9701@naver.com>
2023-07-05 09:15:06 +0900

    Spring-practice

--- EmpPrj/src/main/java/com/example/myapp/hr/IEmpService.java
+++
@@ -1,5 +0,0 @@
-package com.example.myapp.hr;
-
-public interface IEmpService {
-	int getEmpCount(int deptid);
-}
--- MemberPrj/src/main/java/com/sample/myapp/member/IMemberService.java
+++
@@ -1,5 +0,0 @@
-package com.sample.myapp.member;
-
-public interface IMemberService {
-	MemberVO getMemberInfo();
-}
